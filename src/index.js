b56b59a26e6807a53ed49e3f27ea1793ef7f1f16
Shadow073180 <david73180@gmail.com>
2019-12-07 13:29:16 -0500

    last try

--- src/index.js
+++ src/index.js
@@ -5,26 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-var express = require('express');
-var app = express();
-var path = require('path');
 
-app.use(express.static(path.join(PC-frontend)));
-app.use("/styles", express.static(App.css));
-app.use("/images", express.static(__dirname + '/images'));
-app.use("/scripts", express.static(__dirname + '/scripts'));
-
-// viewed at based directory http://localhost:8080/
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + 'views/index.html'));
-});
-
-// add other routes below
-app.get('/about', function (req, res) {
-  res.sendFile(path.join(__dirname + 'views/about.html'));
-});
-
-app.listen(process.env.PORT || 8080);
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
